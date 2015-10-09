a6d10b2ee05144cf0c93cbb0561b35b5d2619389
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-10-09 01:45:42 +0100

    Add damage modifiers.

--- client/app/models/character.js
+++ client/app/models/character.js
@@ -1,6 +1,34 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
+// TODO: Generate this to allow for upward extension.
+const DAMAGE_MODIFIERS = [
+  '-1d8',
+  '-1d6',
+  '-1d4',
+  '-1d2',
+  '+0',
+  '+1d2',
+  '+1d4',
+  '+1d6',
+  '+1d8',
+  '+1d10',
+  '+1d12',
+  '+1d12',
+  '+2d6',
+  '+2d6',
+  '+1d8+1d6',
+  '+1d8+1d6',
+  '+2d8',
+  '+2d8',
+  '+1d10+1d8',
+  '+1d10+1d8',
+  '+2d10+1d2',
+  '+2d10+1d2',
+  '+2d10+1d4',
+  '+2d10+1d4'
+];
+
 export default DS.Model.extend({
   name: DS.attr('string'),
 
@@ -31,5 +59,13 @@ export default DS.Model.extend({
     const actionPoints = Math.ceil((intelligence + dexterity) / 12);
 
     return Math.max(actionPoints, 1);
+  }),
+
+  damageModifier: Ember.computed('strength', 'size', function() {
+    const strength          = parseInt(this.get('strength') || 0);
+    const size              = parseInt(this.get('size') || 0);
+    const damageModifierIdx = Math.max(Math.ceil((strength + size) / 5) - 1, 0);
+
+    return DAMAGE_MODIFIERS[damageModifierIdx];
   })
 });
