9ecd5e51390c7db509d23db6389be3bc0007990d
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2016-05-24 23:03:27 +0100

    Allow setting of the characteristics for skill definitions

--- app/mixins/skill-definition.js
+++ app/mixins/skill-definition.js
@@ -3,12 +3,55 @@ import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
 
 export default Ember.Mixin.create({
+  characteristicNames: [
+    'strength',
+    'constitution',
+    'size',
+    'dexterity',
+    'intelligence',
+    'power',
+    'charisma'
+  ],
+
   ruleSet: belongsTo('rule-set'),
 
   name: attr('string'),
 
-  characteristicA: attr('string'),
-  characteristicB: attr('string'),
+  characteristicA: attr('number'),
+
+  characteristicAName: Ember.computed('characteristicA', {
+    get() {
+      const characteristic = this.get('characteristicA');
+
+      return this.characteristicNames[characteristic];
+    },
+
+    set(key, value) {
+      const characteristic = this.characteristicNames.indexOf(value);
+
+      this.set('characteristicA', characteristic);
+
+      return value;
+    }
+  }),
+
+  characteristicB: attr('number'),
+
+  characteristicBName: Ember.computed('characteristicB', {
+    get() {
+      const characteristic = this.get('characteristicB');
+
+      return this.characteristicNames[characteristic];
+    },
+
+    set(key, value) {
+      const characteristic = this.characteristicNames.indexOf(value);
+
+      this.set('characteristicB', characteristic);
+
+      return value;
+    }
+  }),
 
   baseBonus: attr('number', { defaultValue: 0 }),
 
