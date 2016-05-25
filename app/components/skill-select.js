55e6d98ede8c673b2837cb76c824c256b69067ad
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2016-05-25 02:07:54 +0100

    Sort the skill select skills

--- app/components/skill-select.js
+++ app/components/skill-select.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  skillDefinitionSorting: ['name'],
+  sortedSkillDefinitions: Ember.computed.sort(
+    'skillDefinitions', 'skillDefinitionSorting'
+  ),
+
   actions: {
     selectSkill(skill) {
       this.sendAction('selectSkill', skill);
