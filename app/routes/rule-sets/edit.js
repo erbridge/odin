151537955cd752f7b8f0223c55c85f376185922d
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2016-05-25 02:02:07 +0100

    Pass an edit skill action to the rule set component

--- app/routes/rule-sets/edit.js
+++ app/routes/rule-sets/edit.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('rule-set', params.rule_set_id);
+  },
+
+  actions: {
+    editSkill(skill) {
+      this.transitionTo('skills.edit', skill);
+    }
   }
 });
